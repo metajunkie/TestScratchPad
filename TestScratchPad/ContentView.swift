d2f83b8116991e11404326f9277477e56c027294
Metajunkie <metajunkie@gmail.com>
2020-02-18 18:03:45 -0500

    Initial Commit

---
+++ TestScratchPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestScratchPad
+//
+//  Created by kewal on 2/18/20.
+//  Copyright © 2020 DigiDeeds. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
